af58fc7500289942835190638af30a8fc64b5854
gunkiller <muse520@outlook.com>
2017-08-13 16:16:43 +0800

    添加队列的数据结构

---
+++ Killer.Tools.DataStructure/KillerQueue/KillerQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Killer.Tools.DataStructure.KillerQueue
+{
+    /// <summary>
+    /// 队列数据结构
+    /// </summary>
+    public class KillerQueue<T>
+    {
+        private KillerQueueNode<T> _head;
+        private KillerQueueNode<T> _end;
+        private long _count;
+
+        public long Count { get => _count; set => _count = value; }
+
+        /// <summary>
+        /// 向队列中添加一个数据
+        /// </summary>
+        /// <param name="value"></param>
+        public void Enqueue(T value)
+        {
+            var node = new KillerQueueNode<T>(value);
+            if (this._count == 0)
+            {
+                this._head = node;
+                this._end = node;
+            }
+            else
+            {
+                this._end.NextNode = node;
+                this._end = node;
+            }
+            this._count++;
+        }
+        /// <summary>
+        /// 从对列中取出一个值
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            if (this._count < 1)
+            {
+                throw new IndexOutOfRangeException("队列中的数据为空");
+            }
+            var node = this._head;
+            this._head = this._head.NextNode;
+            node.NextNode = null;
+            this._count--;
+            if (this._count == 0)
+            {
+                this._end = null;
+            }
+            return node.NodeValue;
+        }
+    }
+}
---
+++ Killer.Tools.DataStructure/KillerQueue/KillerQueueNode.cs
@@ -0,0 +1,16 @@
+﻿namespace Killer.Tools.DataStructure.KillerQueue
+{
+    /// <summary>
+    /// 队列数据结构节点
+    /// </summary>
+    internal class KillerQueueNode<T>
+    {
+        internal KillerQueueNode<T> NextNode { get; set; }
+        internal T NodeValue { get; set; }
+        public KillerQueueNode() { }
+        public KillerQueueNode(T value)
+        {
+            this.NodeValue = value;
+        }
+    }
+}
