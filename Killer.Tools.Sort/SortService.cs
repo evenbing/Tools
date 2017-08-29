d0598da212c63af60bc244576ec40f5b8c38cb40
Tools <muse520@outlook.com>
2017-08-29 16:48:18 +0800

    希尔排序

---
+++ Killer.Tools.Sort/SortService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Killer.Tools.Sort
+{
+    /// <summary>
+    /// 排序服务类
+    /// </summary>
+    public class SortService
+    {
+        /// <summary>
+        /// 希尔排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        public static void ShellSort<T>(T[] arr)where T :IComparable<T>
+        {
+            if (arr == null || arr.Length < 2)
+            {
+                return;
+            }
+            int j;
+            for (int d = arr.Length / 2; d >= 1; d = d / 2)
+            {
+                for (int i = d; i < arr.Length; i++)
+                {
+                    j = i - d;
+                    T temp = arr[i];
+
+                    while (j >= 0 && temp.CompareTo(arr[j]) < 0)
+                    {
+                        arr[j + d] = arr[j];
+                        j = j - d;
+                    }
+                    arr[j + d] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ Killer.Tools.Test/Sort/SortServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Killer.Tools.Sort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Killer.Tools.Test.Sort
+{
+    [TestClass]
+    public class SortServiceTest
+    {
+        [TestMethod]
+        public void ShellSortTest()
+        {
+            int[] arr = { 1, 2, 3, 4, 42, 5, 6, 8, 1, 2, 3 };
+
+            SortService.ShellSort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
