0bd5e9a0255cf359555668715075234819c3f4e1
Tools <muse520@outlook.com>
2017-08-30 19:32:25 +0800

    快速排序01

--- Killer.Tools.Sort/SortService.cs
+++ Killer.Tools.Sort/SortService.cs
@@ -14,7 +14,7 @@ namespace Killer.Tools.Sort
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="arr"></param>
-        public static void ShellSort<T>(T[] arr)where T :IComparable<T>
+        public static void ShellSort<T>(T[] arr) where T : IComparable<T>
         {
             if (arr == null || arr.Length < 2)
             {
@@ -36,6 +36,37 @@ namespace Killer.Tools.Sort
                     arr[j + d] = temp;
                 }
             }
+        }
+        /// <summary>
+        /// 快速排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        public static void QuickSort<T>(T[] arr) where T : IComparable<T>
+        {
+            if (arr == null || arr.Length < 2)
+            {
+                return;
+            }
+
+        }
+        private static void QuickSort<T>(T[] arr, int start, int end) where T : IComparable<T>
+        {
+            var temp = arr[start];
+            while (start < end)
+            {
+                while (start < end && arr[end].CompareTo(temp) >= 0)
+                {
+                    end--;
+                }
+                arr[start] = arr[end];
+                while (start < end && arr[start].CompareTo(temp) <= 0)
+                {
+                    start++;
+                }
+                arr[end] = arr[start];
+            }
+            arr[start] = temp;
         }
     }
 }
