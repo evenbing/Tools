7870dff14537d03c12ab78112f8a2dc9eee5acc5
Tools <muse520@outlook.com>
2017-09-01 15:47:55 +0800

    添加 api 全局跨域

---
+++ Killer.Tools.ApiHelp/HttpRequestMessageExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Net.Http;
+
+namespace Killer.Tools.ApiHelp
+{
+    public static class HttpRequestMessageExtensions
+    {
+        /// <summary>
+        /// 是不是预请求
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static bool IsPreRequest(this HttpRequestMessage request)
+        {
+            return request.Method == HttpMethod.Options &&
+               request.Headers.GetValues("Origin").Any() &&
+               request.Headers.GetValues("Access-Control-Request-Method").Any();
+        }
+    }
+}
---
+++ Killer.Tools.ApiHelp/KillerDelegatingHandler/KillerCorsMessageHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+namespace Killer.Tools.ApiHelp.KillerDelegatingHandler
+{
+    /// <summary>
+    /// 自动实现容许跨域
+    /// </summary>
+    public class KillerCorsMessageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            if (!request.IsPreRequest())
+            {
+                response = base.SendAsync(request, cancellationToken).Result;
+
+            }
+            SetResponseHeaders(response, request);
+            return Task.FromResult(response);
+        }
+        private void SetResponseHeaders(HttpResponseMessage responseMessage, HttpRequestMessage requestMessage)
+        {
+            var origin = requestMessage.Headers.GetValues("Origin").FirstOrDefault();
+            if (string.IsNullOrEmpty(origin))
+            {
+                origin = "*";
+            }
+            responseMessage.Headers.Add("Access-Control-Allow-Origin", origin);
+            if (requestMessage.IsPreRequest())
+            {
+                responseMessage.Headers.Add("Access-Control-Allow-Methods", "*");
+                string requestHeaders = requestMessage.Headers.GetValues("Access-Control-Request-Headers").FirstOrDefault();
+                if (!string.IsNullOrEmpty(requestHeaders))
+                {
+                    responseMessage.Headers.Add("Access-Control-Allow-Headers", requestHeaders);
+                }
+            }
+        }
+    }
+}
