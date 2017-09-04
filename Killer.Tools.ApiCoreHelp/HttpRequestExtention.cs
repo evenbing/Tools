9afc153caec124b1c877aa88b085e44613bfa7f6
Tools <muse520@outlook.com>
2017-09-04 15:04:28 +0800

    添加net core 版本

---
+++ Killer.Tools.ApiCoreHelp/HttpRequestExtention.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Killer.Tools.ApiCoreHelp
+{
+    public static class HttpRequestExtention
+    {
+        public static bool IsIsPreRequest(this HttpRequest request)
+        {
+            return request.Method?.ToLowerInvariant() == "options" && request.Headers.Keys.Contains("Origin") && request.Headers.ContainsKey("Access-Control-Request-Method");
+        }
+    }
+}
---
+++ Killer.Tools.ApiCoreHelp/KillerMiddleares/KillerCorsMiddleare.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using System.Threading.Tasks;
+
+namespace Killer.Tools.ApiCoreHelp.KillerMiddleares
+{
+    public class KillerCorsMiddleare
+    {
+        public RequestDelegate Next { get; set; }
+
+
+        public KillerCorsMiddleare(RequestDelegate next)
+        {
+            this.Next = next;
+        }
+        public async Task Invoke(HttpContext context, ILoggerFactory loggerFactory)
+        {
+            await Task.Run(() =>
+            {
+                if (context.Request.IsIsPreRequest())
+                {
+                    context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+                    StringValues head;
+                    if (context.Request.Headers.TryGetValue("Access-Control-Request-Headers", out head))
+                    {
+                        context.Response.Headers.Add("Access-Control-Request-Headers", head);
+                    }
+                }
+                StringValues origin;
+                if (context.Request.Headers.TryGetValue("Origin", out origin))
+                {
+                    context.Response.Headers.Add("Origin", origin);
+                }
+            });
+        }
+    }
+}
